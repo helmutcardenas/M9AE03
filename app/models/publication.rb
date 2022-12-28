474fd8ff8dea5ac26e4183f33eca40ed1720cebb
helmut cardenas <cardenas.hcconstrucciones@gmail.com>
2022-12-27 23:30:45 -0300

    relacion has_many lista

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,2 +1,8 @@
 class Publication < ApplicationRecord
+    # Associations
+    has_many :publication_types, dependent: :destroy
+    has_many :types, through: :publication_types
+
+  # Validations
+  validates :name, :description, presence: true
 end
--- db/migrate/20221228011032_add_tags_to_publication.rb
+++ db/migrate/20221228011032_add_tags_to_publication.rb
@@ -1,4 +1,5 @@
 class AddTagsToPublication < ActiveRecord::Migration[7.0]
   def change
+    add_column :publications, tags, :string, array: true, default
   end
 end
